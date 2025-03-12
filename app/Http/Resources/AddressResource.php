56d80aa4f6b7444c334b1e3c2dc0c9a2c876706f
VickyWijaya2534 <149124588+VickyWijaya2534@users.noreply.github.com>
2025-03-12 17:46:43 +0700

    Add files via upload

---
+++ app/Http/Resources/AddressResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AddressResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'street' => $this->street,
+            'city' => $this->city,
+            'province' => $this->province,
+            'country' => $this->country,
+            'postal_code' => $this->postal_code,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
